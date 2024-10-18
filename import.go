05a752de1caa639edf81aef2646867771ff743fc
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2024-10-18 10:24:50 +0200

    fix memory leak on import

--- import.go
+++ import.go
@@ -93,24 +93,18 @@ func (i *Importer) Add(exportNode *ExportNode) error {
 	// importer in an inconsistent state when we return an error.
 	stackSize := len(i.stack)
 	switch {
-	case stackSize >= 2 && i.stack[stackSize-1].subtreeHeight < node.subtreeHeight && i.stack[stackSize-2].subtreeHeight < node.subtreeHeight:
-		node.leftNode = i.stack[stackSize-2]
-		node.leftHash = node.leftNode.hash
-		node.rightNode = i.stack[stackSize-1]
-		node.rightHash = node.rightNode.hash
-	case stackSize >= 1 && i.stack[stackSize-1].subtreeHeight < node.subtreeHeight:
-		node.leftNode = i.stack[stackSize-1]
-		node.leftHash = node.leftNode.hash
-	}
-
-	if node.subtreeHeight == 0 {
+	case node.subtreeHeight == 0:
 		node.size = 1
-	}
-	if node.leftNode != nil {
-		node.size += node.leftNode.size
-	}
-	if node.rightNode != nil {
-		node.size += node.rightNode.size
+	case stackSize >= 2 && i.stack[stackSize-1].subtreeHeight < node.subtreeHeight && i.stack[stackSize-2].subtreeHeight < node.subtreeHeight:
+		leftNode := i.stack[stackSize-2]
+		rightNode := i.stack[stackSize-1]
+		node.leftHash = leftNode.hash
+		node.rightHash = rightNode.hash
+		node.size = leftNode.size + rightNode.size
+	case stackSize >= 1 && i.stack[stackSize-1].subtreeHeight < node.subtreeHeight:
+		leftNode := i.stack[stackSize-1]
+		node.leftHash = leftNode.hash
+		node.size = leftNode.size
 	}
 
 	_, err := node._hash()
